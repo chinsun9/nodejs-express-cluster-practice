ac2a620cb88a28dbb57de61034ac70cbd877fa35
chinsun9 <40482203+chinsun9@users.noreply.github.com>
2020-10-21 16:45:29 +0900

    Create web server

--- my-react/server/index.js
+++ my-react/server/index.js
@@ -1,17 +1,3 @@
-# nodejs-express-cluster-practice
-
-- https://smoh.tistory.com/m/339?category=694831 보고 따라하기
-
-## 웹서버 생성
-
-```
-npx create-react-app my-react
-cd my-react
-
-npm i express express-favicon
-```
-
-```js my-react/server/index.js
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
@@ -32,23 +18,3 @@ app.get('/ping', (req, res) => {
 app.get('/*', (req, res) => {
   return res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
-```
-
-- 서버 코드 작성
-
-```cmd cmd
-npm run build
-```
-
-- 빌드
-
-```cmd cmd
-node server
-```
-
-- 서버 실행
-
-  http://localhost:3000/
-  http://localhost:3000/ping
-
-- 잘 서빙하는지 확인
